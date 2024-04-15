eccd25f8a2cf55b862fe7b579b56bc9be78bf1cb
LastCow9000 <sys19912002@hanmail.net>
2024-04-15 20:53:01 +0900

    feat: 학교 페이지 소식 삭제 기능

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { NewsService } from './news.service';
 import { JwtAdminGuard } from 'src/auth/guards/jwt-admin.guard';
 import { User } from 'src/common/decorators/user.decorator';
@@ -12,5 +20,11 @@ export class NewsController {
   @UseGuards(JwtAdminGuard)
   createNews(@Body() createNewsDto: CreateNewsDto, @User() user) {
     return this.newsService.createNews(createNewsDto, user.id);
+  }
+
+  @Delete('/:news_id')
+  @UseGuards(JwtAdminGuard)
+  deleteNews(@Param('news_id', ParseIntPipe) newsId: number, @User() user) {
+    return this.newsService.deleteNews(newsId, user.id);
   }
 }
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { News } from 'src/entities';
 import { Repository } from 'typeorm';
@@ -14,15 +18,38 @@ export class NewsService {
   ) {}
 
   async createNews({ title, content }: CreateNewsDto, adminId: number) {
+    const school = await this.validateManagedSchool(adminId);
+    const news = this.newsRepository.create({ title, content, school });
+
+    return await this.newsRepository.save(news);
+  }
+
+  async deleteNews(newsId: number, adminId: number) {
+    const school = await this.validateManagedSchool(adminId);
+    const existNews = await this.newsRepository.findOne({
+      where: { id: newsId },
+      relations: ['school'],
+    });
+    if (!existNews) {
+      throw new NotFoundException('해당하는 소식이 존재하지 않습니다.');
+    }
+    if (existNews.school.id !== school.id) {
+      throw new ForbiddenException('다른 학교의 소식은 삭제할 수 없습니다.');
+    }
+
+    await this.newsRepository.delete(newsId);
+
+    return newsId;
+  }
+
+  async validateManagedSchool(adminId: number) {
     const admin = await this.adminsService.findAdminById(adminId);
     if (!admin.managedSchool) {
       throw new ForbiddenException(
         '관리 중인 학교 페이지가 존재하지 않습니다.',
       );
     }
-    const school = admin.managedSchool;
 
-    const news = this.newsRepository.create({ title, content, school });
-    return await this.newsRepository.save(news);
+    return admin.managedSchool;
   }
 }
